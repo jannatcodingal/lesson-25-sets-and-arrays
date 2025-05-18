20fed1eecf0489eb5a479ca64e14daf5d74fc08a
jannatcodingal <jannatrehman007@gmail.com>
2025-05-18 19:26:24 +0500

    intersection

---
+++ intersection of sets.py
@@ -0,0 +1,8 @@
+seta={"green","blue"}
+setb={"yellow","blue"}
+print("original set elements: ")
+print(seta)
+print(setb)
+print('intersection of two set elements: ')
+setz=seta.intersection(setb)
+print(setz)
